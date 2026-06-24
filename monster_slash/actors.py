7862c8f0a30fbdccd32a0869ab7a1b8e0be42c38
TusharCodex-Web <tushar23jangid@gmail.com>
2026-06-24 22:14:10 +0530

    Update actors.py

--- monster_slash/actors.py
+++ monster_slash/actors.py
@@ -1,6 +1,6 @@
 from random import randint
 
-class player:
+class Player:
     def __init__(self, name, level):
         self.name = name
         self.level = level
@@ -11,6 +11,20 @@ class player:
     
     def get_attack_power(self):
         return randint(1, 100) * self.level
+    
+    def attacks(self, enemy):
+        power = self.get_attack_power()
+        enemy_power = enemy.get_attack_power()
+
+        print('you summon {} power units!'.format(power))
+        print('{} summon {} power units!'.format(enemy.kind, enemy_power))
+
+        if power >= enemy_power:
+            print('you slay the {}'.format(enemy.kind))
+            return True
+        else:
+            print('you were defeated!')
+            return False
     
 class Enemy:
     def __init__(self, kind, level):
@@ -24,9 +38,9 @@ class Enemy:
         return randint(1, 100) * self.level
     
 if __name__ == '__main__':
-    # player1 = player(name='Batman', level=1)
-    # print(player1)
-    # print(player1.get_attack_power())
-    enemy1 = Enemy(kind='Joker', level=1)
-    print(enemy1)
-    print(enemy1.get_attack_power())
+    player = player(name='Hercules', level=1)
+    print(player)
+    print(player.get_attack_power())
+    enemy = Enemy(kind='Joker', level=1)
+    print(enemy)
+    print(enemy.get_attack_power())
