4ef02f7554eb38001afb5984b42ad3f4431f1334
TusharCodex-Web <tushar23jangid@gmail.com>
2026-06-25 22:07:18 +0530

    Update actors.py

--- monster_slash/actors.py
+++ monster_slash/actors.py
@@ -1,16 +1,21 @@
 from random import randint
 
-class Player:
+class Actor:
     def __init__(self, name, level):
         self.name = name
         self.level = level
-
-    def __repr__(self):
-        return '<player {} at level {}>'.format(self.name,
-                                                   self.level)
     
+    def __repr__(self):
+        return '<Actor: {}, Level:{}'.format(self.name, self.level)
+
     def get_attack_power(self):
-        return randint(1, 100) * self.level
+        return randint(1,100) * self.level
+    
+    def attacks(self, other):
+        raise NotImplementedError()
+    
+
+class Player(Actor):
     
     def attacks(self, enemy):
         power = self.get_attack_power()
@@ -26,21 +31,31 @@ class Player:
             print('you were defeated!')
             return False
     
-class Enemy:
-    def __init__(self, kind, level):
+class Enemy(Actor):
+    def __init__(self, name, level , kind):
+        super().__init__(name, level)
         self.kind = kind
-        self.level = level
 
-    def __repr__(self):
-        return '<Enemy: {}>'.format(self.kind)
-    
+class Ogre(Enemy):
+    def __init__(self, name, level, size):
+        super().__init__(name, level, 'Ogre')
+        self.size = size
+
     def get_attack_power(self):
-        return randint(1, 100) * self.level
+        return randint(1,50) * (self.size * self.level)
+
+class Imp(Enemy):
+    def __init__(self, name, level):
+        super().__init__(name, level, 'Imp')
+
+    def get_attack_power(self):
+        return super().get_attacks_power() / 4
+    
     
 if __name__ == '__main__':
-    player = player(name='Hercules', level=1)
-    print(player)
-    print(player.get_attack_power())
+    # player = player(name='Hercules', level=1)
+    # print(player)
+    # print(player.get_attack_power())
     enemy = Enemy(kind='Joker', level=1)
     print(enemy)
     print(enemy.get_attack_power())
